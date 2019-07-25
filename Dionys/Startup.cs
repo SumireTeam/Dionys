838ee7d16363e13193bfe475981ccb5003d64de1
Vladislav Dyshakov <techiedesu@protonmail.com>
2019-07-26 04:10:22 +0700

    Project structure refactoring

--- Dionys/Startup.cs
+++ Dionys/Startup.cs
@@ -1,5 +1,8 @@
+using System.IO;
+using System.Text;
 using AutoMapper;
 using Dionys.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +10,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 
@@ -24,12 +28,38 @@ namespace Dionys
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer           = true,
+                        ValidateAudience         = true,
+                        ValidateLifetime         = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer   = "http://localhost:5000",
+                        ValidAudience = "http://localhost:5000",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+                    };
+                });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
+                // Configuration.GetConnectionString("DefaultConnection")
                 configuration.RootPath = "ClientApp/build";
+            });
+
+            // CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build();
+                });
             });
 
             services.AddDbContext<DionysContext>(options => options.UseNpgsql(
@@ -49,7 +79,6 @@ namespace Dionys
 
             var mappingConfig = new MapperConfiguration(mc =>
             {
-                // MappingScenario
                 mc.AddProfile(sp.GetService<MappingScenario>());
             });
 
@@ -69,8 +98,10 @@ namespace Dionys
                 app.UseExceptionHandler("/Error");
             }
 
+            app.UseAuthentication();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+            app.UseCors("EnableCORS");
 
             app.UseMvc(routes =>
             {
