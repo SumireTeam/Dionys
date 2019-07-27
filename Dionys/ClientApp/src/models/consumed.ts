4108d7f577d6e97149d6c57fd8fae6c7e0e4a83d
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2019-07-27 12:34:25 +0700

    Show product name on the consumed product pages

--- Dionys/ClientApp/src/models/consumed.ts
+++ Dionys/ClientApp/src/models/consumed.ts
@@ -1,6 +1,9 @@
+import { Product } from './product';
+
 export interface Consumed {
   readonly id: string;
   readonly productId: string;
+  readonly product?: Product;
   readonly weight: number;
   readonly date: Date;
 }
