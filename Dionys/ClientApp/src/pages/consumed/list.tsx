6e8ad469445dbf24503cebc6f43c08ca14689b20
Nina Lisitsinskaya <vvatashi4259@gmail.com>
2019-07-27 11:05:19 +0700

    Add delete action & dialog to lists

--- Dionys/ClientApp/src/pages/consumed/list.tsx
+++ Dionys/ClientApp/src/pages/consumed/list.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Breadcrumbs, CircularProgress, Button } from '@material-ui/core';
+import {
+  Breadcrumbs,
+  CircularProgress,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@material-ui/core';
 import { Layout, Link, ConsumedList, LinkAdapter } from '../../components';
 import { ServiceProvider } from '../../services';
 import { Consumed } from '../../models';
@@ -7,22 +16,60 @@ import { Consumed } from '../../models';
 interface State {
   readonly loading: boolean;
   readonly consumed: Consumed[];
+  readonly deleteDialogItem: null | Consumed;
+  readonly deleteDialogOpen: boolean;
 }
 
 class List extends React.Component<{}, State> {
+  protected readonly service = ServiceProvider.consumedService;
+
   public constructor(props) {
     super(props);
 
     this.state = {
       loading: true,
       consumed: [],
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
     };
+
+    this.openDeleteDialog = this.openDeleteDialog.bind(this);
+    this.dismissDeleteDialog = this.dismissDeleteDialog.bind(this);
+    this.confirmDeleteDialog = this.confirmDeleteDialog.bind(this);
   }
 
   public async componentDidMount() {
-    const service = ServiceProvider.consumedService;
-    const consumed = await service.list();
-    this.setState({ loading: false, consumed });
+    const consumed = await this.service.list();
+
+    this.setState({
+      loading: false,
+      consumed,
+    });
+  }
+
+  protected openDeleteDialog(item: Consumed) {
+    this.setState({
+      deleteDialogItem: item,
+      deleteDialogOpen: true,
+    });
+  }
+
+  protected dismissDeleteDialog() {
+    this.setState({
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
+    });
+  }
+
+  protected async confirmDeleteDialog() {
+    const id = this.state.deleteDialogItem.id;
+    await this.service.delete(id);
+
+    this.setState({
+      consumed: this.state.consumed.filter(item => item.id !== id),
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
+    });
   }
 
   public render() {
@@ -42,7 +89,22 @@ class List extends React.Component<{}, State> {
 
         {this.state.loading
           ? <CircularProgress className="progress" />
-          : <ConsumedList consumed={this.state.consumed} />}
+          : <ConsumedList consumed={this.state.consumed} openDeleteDialog={this.openDeleteDialog} />}
+
+        <Dialog open={this.state.deleteDialogOpen} onClose={e => this.dismissDeleteDialog()}>
+          <DialogTitle>Delete consumed product</DialogTitle>
+
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to delete consumed product?
+            </DialogContentText>
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={e => this.dismissDeleteDialog()} color="primary">No</Button>
+            <Button onClick={e => this.confirmDeleteDialog()} color="primary" autoFocus>Yes</Button>
+          </DialogActions>
+        </Dialog>
       </Layout>
     );
   };
--- Dionys/ClientApp/src/pages/product/list.tsx
+++ Dionys/ClientApp/src/pages/product/list.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Breadcrumbs, CircularProgress, Button } from '@material-ui/core';
+import {
+  Breadcrumbs,
+  CircularProgress,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@material-ui/core';
 import { Layout, Link, ProductList, LinkAdapter } from '../../components';
 import { ServiceProvider } from '../../services';
 import { Product } from '../../models';
@@ -7,22 +16,60 @@ import { Product } from '../../models';
 interface State {
   readonly loading: boolean;
   readonly products: Product[];
+  readonly deleteDialogItem: null | Product;
+  readonly deleteDialogOpen: boolean;
 }
 
 class List extends React.Component<{}, State> {
+  protected readonly service = ServiceProvider.productService;
+
   public constructor(props) {
     super(props);
 
     this.state = {
       loading: true,
       products: [],
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
     };
+
+    this.openDeleteDialog = this.openDeleteDialog.bind(this);
+    this.dismissDeleteDialog = this.dismissDeleteDialog.bind(this);
+    this.confirmDeleteDialog = this.confirmDeleteDialog.bind(this);
   }
 
   public async componentDidMount() {
-    const service = ServiceProvider.productService;
-    const products = await service.list();
-    this.setState({ loading: false, products });
+    const products = await this.service.list();
+
+    this.setState({
+      loading: false,
+      products,
+    });
+  }
+
+  protected openDeleteDialog(item: Product) {
+    this.setState({
+      deleteDialogItem: item,
+      deleteDialogOpen: true,
+    });
+  }
+
+  protected dismissDeleteDialog() {
+    this.setState({
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
+    });
+  }
+
+  protected async confirmDeleteDialog() {
+    const id = this.state.deleteDialogItem.id;
+    await this.service.delete(id);
+
+    this.setState({
+      products: this.state.products.filter(item => item.id !== id),
+      deleteDialogItem: null,
+      deleteDialogOpen: false,
+    });
   }
 
   public render() {
@@ -42,7 +89,22 @@ class List extends React.Component<{}, State> {
 
         {this.state.loading
           ? <CircularProgress className="progress" />
-          : <ProductList products={this.state.products} />}
+          : <ProductList products={this.state.products} openDeleteDialog={this.openDeleteDialog} />}
+
+        <Dialog open={this.state.deleteDialogOpen} onClose={e => this.dismissDeleteDialog()}>
+          <DialogTitle>Delete product</DialogTitle>
+
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to delete product?
+            </DialogContentText>
+          </DialogContent>
+
+          <DialogActions>
+            <Button onClick={e => this.dismissDeleteDialog()} color="primary">No</Button>
+            <Button onClick={e => this.confirmDeleteDialog()} color="primary" autoFocus>Yes</Button>
+          </DialogActions>
+        </Dialog>
       </Layout>
     );
   };
