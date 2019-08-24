a983c7b401753ed662847fcb3a6d1e36ec2129d6
Vlad <techiedesu@protonmail.com>
2019-08-24 15:09:51 +0700

    Updated eslint.

--- Dionys.Web/ClientApp/.eslintrc.js
+++ Dionys.Web/ClientApp/.eslintrc.js
@@ -1,24 +1,29 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: [
+  "extends": [
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
+    'plugin:react/recommended'
   ],
   rules: {
     '@typescript-eslint/indent': [2, 2],
     '@typescript-eslint/explicit-function-return-type': 0,
+    'quotes': [2, 'single', { 'avoidEscape': true }]
   },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
+  "env": {
+    "browser": true,
+    "node": true
+  }
 };
